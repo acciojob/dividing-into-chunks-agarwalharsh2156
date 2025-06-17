fb2492f896db393162a29837cb3b8bdb45225674
Harsh Agarwal <agarwalharsh2156@gmail.com>
2025-06-17 22:04:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  let result = [];
+  let currentChunk = [];
+  let currentSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (currentSum + arr[i] <= n) {
+      currentChunk.push(arr[i]);
+      currentSum += arr[i];
+    } else {
+      // Push current chunk and start a new one
+      result.push(currentChunk);
+      currentChunk = [arr[i]];
+      currentSum = arr[i];
+    }
 };
 
 const n = prompt("Enter n: ");
